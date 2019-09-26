feda7fd508b0f4a858c27c5569fe6a787fbbe11e
bluebubblewei <weiyumei@yzsci.cn>
2019-09-26 10:06:38 +0800

    初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import homePage from '@/components/homePage'
 
 Vue.use(Router)
@@ -8,17 +7,12 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
       path: '',
       component: homePage,
       redirect: 'home',
       children: [{
         path: 'home',
-        component: () => import('@/components/homePage'),
+        component: () => import('@/views/homePage'),
         name: '首页',
         meta: {
           title: '首页',
@@ -28,6 +22,26 @@ export default new Router({
         },
         hidden: false
       }]
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/login/index'),
+      hidden: true
+    },
+    {
+      path: '/authredirect',
+      component: () => import('@/views/login/auth-redirect'),
+      hidden: true
+    },
+    {
+      path: '/404',
+      component: () => import('@/views/errorPage/404'),
+      hidden: true
+    },
+    {
+      path: '/401',
+      component: () => import('@/views/errorPage/401'),
+      hidden: true
     }
   ]
 })
---
+++ src/views/login/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h3>
+请登录
+</h3>
+
+</template>
+<script>
+export default {
+
+}
+</script>
